4b7b304e8700ddf3a3ee7f63b3fb68e032f9f6a2
Alexandre <alexandre.ayotte2@USherbrooke.ca>
2020-12-05 11:28:02 -0500

    ResNet: Mixup handle and typing added

--- Model/ResNet.py
+++ Model/ResNet.py
@@ -2,6 +2,7 @@ from Model.Module import Mixup
 from monai.networks.blocks.convolutions import Convolution, ResidualUnit
 from monai.networks.layers.factories import Act
 import numpy as np
+from random import randint
 import torch
 import torch.nn as nn
 from typing import Sequence, Tuple, Union
@@ -38,7 +39,7 @@ class PreResBlock(nn.Module):
                                       groups=groups if split_layer is False else 1)
 
         self.bn1 = nn.BatchNorm3d(fmap_in)
-        self.activation1 = nn.ReLU()
+        self.activation1 = Act[activation]()
 
         if type(kernel) == int:
             padding = int((kernel - 1)/2)
@@ -50,7 +51,7 @@ class PreResBlock(nn.Module):
                       padding=padding, bias=True,
                       groups=groups if split_layer is False else 1,),
             nn.BatchNorm3d(fmap_out),
-            nn.ReLU(),
+            Act[activation](),
             nn.Conv3d(fmap_out, fmap_out, kernel_size=kernel, stride=1,
                       padding=padding, bias=True,
                       groups=groups),
@@ -62,7 +63,7 @@ class PreResBlock(nn.Module):
 
         self.residual_layer = nn.Sequential(*res_layer)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         Define the forward pass of the Residual layer
 
@@ -108,7 +109,7 @@ class ResBlock(nn.Module):
                                 dropout_dim=3, act=activation, norm="batch",
                                 last_conv_only=False)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         out = self.res(x)
 
         return out
@@ -138,10 +139,10 @@ class PreResBlock2(nn.Module):
         self.act = Act[activation]()
         self.res = ResidualUnit(dimensions=3, in_channels=fmap_in, out_channels=fmap_out,
                                 kernel_size=kernel, strides=strides, dropout=drop_rate,
-                                dropout_dim=2, act=activation, norm="INSTANCE",
+                                dropout_dim=2, act=activation, norm="BATCH",
                                 last_conv_only=True)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         out = self.bn(x)
         out = self.act(out)
         out = self.res(out)
@@ -150,6 +151,36 @@ class PreResBlock2(nn.Module):
 
 
 class ResNet(nn.Module):
+    """
+    Create a pre activation or post activation 3D Residual Network.
+     ...
+    Attributes
+    ----------
+    conv: Convolution
+        First block of the network. If pre_act is True then, its only a convolution. Else, its combination of
+        convolution, activation et normalisation.
+    fc_layer: nn.Linear
+        The last fully connected layer.
+    __in_channels: int
+        Number of output channels of the last convolution created. Used to determine the number of input channels of
+        the next convolution to create.
+    layers1: nn.Sequential
+        The first series of residual block.
+    layers2: nn.Sequential
+        The second series of residual block.
+    layers3: nn.Sequential
+        The third series of residual block.
+    layers4: nn.Sequential
+        The last series of residual block.
+    mixup: nn.ModuleDict
+        A dictionnary that contain all the mixup module.
+    __num_flat_features: int
+        Number of features at the output of the last convolution.
+    Methods
+    -------
+    forward(x: torch.Tensor) -> torch.Tensor
+        Execute the forward on a given torch.Tensor.
+    """
     def __init__(self, depth: int = 18, first_channels: int = 16,
                  num_classes: int = 2,
                  in_shape: Union[Sequence[int], Tuple] = (16, 64, 64),
@@ -206,28 +237,28 @@ class ResNet(nn.Module):
         # --------------------------------------------
         #                  CONV LAYERS
         # --------------------------------------------
-        self.in_channels = first_channels
+        self.__in_channels = first_channels
         self.conv = Convolution(dimensions=3,
                                 in_channels=3,
-                                out_channels=self.in_channels,
+                                out_channels=self.__in_channels,
                                 kernel_size=first_kernel,
                                 act=act,
                                 conv_only=~pre_act)
 
         self.layers1 = self.__make_layer(block[depth], layers[depth][0],
-                                         self.in_channels,
+                                         self.__in_channels,
                                          kernel=kernel, strides=[2, 2, 1],
                                          drop_rate=dropout[0], act=act)
         self.layers2 = self.__make_layer(block[depth], layers[depth][1],
-                                         self.in_channels * 2,
+                                         self.__in_channels * 2,
                                          kernel=kernel, strides=[2, 2, 2],
                                          drop_rate=dropout[1], act=act)
         self.layers3 = self.__make_layer(block[depth], layers[depth][2],
-                                         self.in_channels * 2,
+                                         self.__in_channels * 2,
                                          kernel=kernel, strides=[2, 2, 2],
                                          drop_rate=dropout[2], act=act)
         self.layers4 = self.__make_layer(block[depth], layers[depth][3],
-                                         self.in_channels * 2,
+                                         self.__in_channels * 2,
                                          kernel=kernel, strides=[2, 2, 2],
                                          drop_rate=dropout[3], act=act)
 
@@ -237,27 +268,27 @@ class ResNet(nn.Module):
         in_shape = list(in_shape)
         out_shape = int((in_shape[0] / 16) * (in_shape[1] / 16) * (in_shape[2] / 8))
 
-        self.num_flat_features = out_shape * self.in_channels
+        self.__num_flat_features = out_shape * self.__in_channels
 
-        self.fc_layer = torch.nn.Sequential(torch.nn.Linear(self.num_flat_features, num_classes))
+        self.fc_layer = nn.Linear(self.__num_flat_features, num_classes)
 
     def __make_layer(self, block, num_block: int, fmap_out: int,
                      kernel: Union[Sequence[int], int],
                      strides: Union[Sequence[int], int] = 1,
-                     drop_rate: Sequence[float] = None, act: str = "ReLU"):
+                     drop_rate: Sequence[float] = None, act: str = "ReLU") -> nn.Sequential:
         layers = []
 
         for i in range(num_block):
-            layers.append(block(fmap_in=self.in_channels, fmap_out=fmap_out,
+            layers.append(block(fmap_in=self.__in_channels, fmap_out=fmap_out,
                                 kernel=kernel,
                                 strides=strides if i == 0 else 1,
                                 drop_rate=drop_rate[i],
                                 activation=act))
-            self.in_channels = fmap_out * block.expansion if i == 0 else self.in_channels
+            self.__in_channels = fmap_out * block.expansion if i == 0 else self.__in_channels
 
         return nn.Sequential(*layers)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         mixup_key_list = list(self.mixup.keys())
 
         out = self.mixup[0](x) if 0 in mixup_key_list else x
@@ -273,13 +304,87 @@ class ResNet(nn.Module):
         out = self.mixup[3](out) if 3 in mixup_key_list else out
         out = self.layers4(out)
 
-        features = out.view(-1, self.num_flat_features)
+        features = out.view(-1, self.__num_flat_features)
         out = self.fc_layer(features)
 
         return out
 
+    def set_mixup(self, b_size: int):
+        """
+        Set the b_size parameter of each mixup module.
+
+        :param b_size: An integer that represent the batch size parameter.
+        """
+        for module in self.mixup.values():
+            module.set_bacth_size(b_size)
+
+    def activate_mixup(self) -> Tuple[int, float, Sequence[int]]:
+        """
+        Choose randomly a mixup module and activate it.
+        """
+        key_list = list(self.mixup.keys())
+        rand_key = key_list[randint(0, len(key_list) - 1)]
+
+        lamb, permut = self.mixup[rand_key].sample()
+        return rand_key, lamb, permut
+
+    def disable_mixup(self, key: int = -1):
+        """
+        Disable one or all mixup module.
+
+        :param key: The name of the mixup module in the dictionary self.mixup.
+                    If key=-1 then all mixup module will be disable.
+        """
+        if key == -1:
+            for module in self.mixup.values():
+                module.enable = False
+        else:
+            self.mixup[key].enable = False
+
 
 class MultiLevelResNet(nn.Module):
+    """
+    Create a pre activation or post activation 3D Residual Network.
+     ...
+    Attributes
+    ----------
+    conv: Convolution
+        First block of the network. If pre_act is True then, its only a convolution. Else, its combination of
+        convolution, activation et normalisation.
+    fc_layer_mal: nn.Linear
+        The last fully connected layer that will be used to classify the malignity of the tumor.
+    fc_layer_sub_1: nn.Linear
+        The first fully connected layer that will be used to classify the subtype of the tumor.
+    fc_layer_sub_2: nn.Linear
+        The last fully connected layer that will be used to classify the subtype of the tumor.
+    fc_layer_grade_1: nn.Linear
+        The first fully connected layer that will be used to classify the grade of the tumor.
+    fc_layer_grade_2: nn.Linear
+        The last fully connected layer that will be used to classify the grade of the tumor.
+    __in_channels: int
+        Number of output channels of the last convolution created. Used to determine the number of input channels of
+        the next convolution to create.
+    layers1: nn.Sequential
+        The first series of residual block.
+    layers2: nn.Sequential
+        The second series of residual block.
+    layers3: nn.Sequential
+        The third series of residual block.
+    layers4: nn.Sequential
+        The last series of residual block.
+    mixup: nn.ModuleDict
+        A dictionnary that contain all the mixup module.
+    __num_flat_features: int
+        Number of features at the output of the last convolution.
+    __split: int
+        Indicate where the network will be split to a multi-task network. Should be an integer between 1 and 5.
+        1 indicate that the network will be split before the first series of residual block.
+        5 indicate that the network will be split after the last series of residual block.
+    Methods
+    -------
+    forward(x: torch.Tensor) -> torch.Tensor
+        Execute the forward on a given torch.Tensor.
+    """
     def __init__(self, depth: int = 18, first_channels: int = 16,
                  split_level: int = 4,
                  in_shape: Union[Sequence[int], Tuple] = (16, 64, 64),
@@ -291,8 +396,8 @@ class MultiLevelResNet(nn.Module):
 
         super().__init__()
         self.mixup = nn.ModuleDict()
-        self.split = split_level
-        self.in_channels = first_channels
+        self.__split = split_level
+        self.__in_channels = first_channels
         # --------------------------------------------
         #                   MIXUP
         # --------------------------------------------
@@ -340,31 +445,31 @@ class MultiLevelResNet(nn.Module):
         assert 1 <= split_level <= 5, "The split level should be an integer between 1 and 5."
         self.conv = Convolution(dimensions=3,
                                 in_channels=3,
-                                out_channels=self.in_channels,
+                                out_channels=self.__in_channels,
                                 kernel_size=first_kernel,
                                 act=act,
                                 conv_only=~pre_act)
 
         self.layers1 = self.__make_layer(block[depth], layers[depth][0],
-                                         self.in_channels,
+                                         self.__in_channels,
                                          kernel=kernel, strides=[2, 2, 1],
                                          drop_rate=dropout[0], act=act,
                                          split_layer=(1 == split_level),
                                          groups=3 if 1 >= split_level else 1)
         self.layers2 = self.__make_layer(block[depth], layers[depth][1],
-                                         self.in_channels * 2,
+                                         self.__in_channels * 2,
                                          kernel=kernel, strides=[2, 2, 2],
                                          drop_rate=dropout[1], act=act,
                                          split_layer=(2 == split_level),
                                          groups=3 if 2 >= split_level else 1)
         self.layers3 = self.__make_layer(block[depth], layers[depth][2],
-                                         self.in_channels * 2,
+                                         self.__in_channels * 2,
                                          kernel=kernel, strides=[2, 2, 2],
                                          drop_rate=dropout[2], act=act,
                                          split_layer=(3 == split_level),
                                          groups=3 if 3 >= split_level else 1)
         self.layers4 = self.__make_layer(block[depth], layers[depth][3],
-                                         self.in_channels * 2,
+                                         self.__in_channels * 2,
                                          kernel=kernel, strides=[2, 2, 2],
                                          drop_rate=dropout[3], act=act,
                                          split_layer=(4 == split_level),
@@ -376,17 +481,17 @@ class MultiLevelResNet(nn.Module):
         in_shape = list(in_shape)
         out_shape = int((in_shape[0] / 16) * (in_shape[1] / 16) * (in_shape[2] / 8))
 
-        if self.split == 5:
-            self.num_flat_features = out_shape * self.in_channels
+        if self.__split == 5:
+            self.__num_flat_features = out_shape * self.__in_channels
         else:
-            self.num_flat_features = out_shape * int(self.in_channels / 3)
+            self.__num_flat_features = out_shape * int(self.__in_channels / 3)
 
-        self.fc_layer_mal = torch.nn.Sequential(torch.nn.Linear(self.num_flat_features, 2))
+        self.fc_layer_mal = torch.nn.Sequential(torch.nn.Linear(self.__num_flat_features, 2))
 
-        self.fc_layer_sub_1 = torch.nn.Sequential(torch.nn.Linear(self.num_flat_features, 3))
+        self.fc_layer_sub_1 = torch.nn.Sequential(torch.nn.Linear(self.__num_flat_features, 3))
         self.fc_layer_sub_2 = torch.nn.Sequential(torch.nn.Linear(5, 3))
 
-        self.fc_layer_grade_1 = torch.nn.Sequential(torch.nn.Linear(self.num_flat_features, 3))
+        self.fc_layer_grade_1 = torch.nn.Sequential(torch.nn.Linear(self.__num_flat_features, 3))
         self.fc_layer_grade_2 = torch.nn.Sequential(torch.nn.Linear(5, 3))
 
     def __make_layer(self, block, num_block: int, fmap_out: int,
@@ -394,12 +499,12 @@ class MultiLevelResNet(nn.Module):
                      strides: Union[Sequence[int], int] = 1,
                      drop_rate: Sequence[float] = None, act: str = "ReLU",
                      groups: int = 1,
-                     split_layer: bool = False):
+                     split_layer: bool = False) -> nn.Sequential:
 
         fmap_out = fmap_out * 3 if split_layer else fmap_out
         layers = []
         for i in range(num_block):
-            layers.append(block(fmap_in=self.in_channels, fmap_out=fmap_out,
+            layers.append(block(fmap_in=self.__in_channels, fmap_out=fmap_out,
                                 kernel=kernel,
                                 strides=strides if i == 0 else 1,
                                 drop_rate=drop_rate[i],
@@ -407,11 +512,11 @@ class MultiLevelResNet(nn.Module):
                                 groups=groups,
                                 split_layer=split_layer))
             split_layer = False
-            self.in_channels = fmap_out * block.expansion if i == 0 else self.in_channels
+            self.__in_channels = fmap_out * block.expansion if i == 0 else self.__in_channels
 
         return nn.Sequential(*layers)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         mixup_key_list = list(self.mixup.keys())
 
         out = self.mixup[0](x) if 0 in mixup_key_list else x
@@ -427,15 +532,15 @@ class MultiLevelResNet(nn.Module):
         out = self.mixup[3](out) if 3 in mixup_key_list else out
         out = self.layers4(out)
 
-        if self.split == 5:
-            features = out.view(-1, self.num_flat_features)
+        if self.__split == 5:
+            features = out.view(-1, self.__num_flat_features)
 
             mal_pred = self.fc_layer_mal(features)
             out_sub = self.fc_layer_sub_1(features)
             out_grade = self.fc_layer_grade_1(features)
 
         else:
-            features = out.view(-1, 3, self.num_flat_features)
+            features = out.view(-1, 3, self.__num_flat_features)
 
             mal_pred = self.fc_layer_mal(features[:, 0, :])
             out_sub = self.fc_layer_sub_1(features[:, 1, :])
@@ -445,3 +550,35 @@ class MultiLevelResNet(nn.Module):
         grade_pred = self.fc_layer_grade_2(torch.cat((out_grade, mal_pred), dim=1))
 
         return mal_pred, sub_pred, grade_pred
+
+    def set_mixup(self, b_size: int):
+        """
+        Set the b_size parameter of each mixup module.
+
+        :param b_size: An integer that represent the batch size parameter.
+        """
+        for module in self.mixup.values():
+            module.set_bacth_size(b_size)
+
+    def activate_mixup(self) -> Tuple[int, float, Sequence[int]]:
+        """
+        Choose randomly a mixup module and activate it.
+        """
+        key_list = list(self.mixup.keys())
+        rand_key = key_list[randint(0, len(key_list) - 1)]
+
+        lamb, permut = self.mixup[rand_key].sample()
+        return rand_key, lamb, permut
+
+    def disable_mixup(self, key: int = -1):
+        """
+        Disable one or all mixup module.
+
+        :param key: The name of the mixup module in the dictionary self.mixup.
+                    If key=-1 then all mixup module will be disable.
+        """
+        if key == -1:
+            for module in self.mixup.values():
+                module.enable = False
+        else:
+            self.mixup[key].enable = False
