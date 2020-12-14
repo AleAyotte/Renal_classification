83e8aa7a183197e4c4ed46d750503981a8c2494e
Alexandre Ayotte <alexandre.ayotte2@Usherbrooke.ca>
2020-12-14 17:10:58 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,21 +1,23 @@
 from Data_manager.DataManager import RenalDataset
+from matplotlib import pyplot as plt
 from Model.ResNet import MultiLevelResNet
-from monai.transforms import RandFlipd, ScaleIntensityd, ToTensord, Compose
+from monai.transforms import RandFlipd, ScaleIntensityd, ToTensord, Compose, AddChanneld
+from torchsummary import summary
 from Trainer.Trainer import Trainer
 
-data_path = "/home/local/USHERBROOKE/ayoa2402/Maitrise/Renal_classification/final_dtset/Option2_without_N4/all.hdf5"
+
+data_path = "final_dtset/Option1_with_N4/all.hdf5"
 
 transform = Compose([
-    RandFlipd(keys=["t1", "t2", "roi"], spatial_axis=[1, 2]),
+    AddChanneld(keys=["t1", "t2", "roi"]),
+    RandFlipd(keys=["t1", "t2", "roi"], spatial_axis=[0, 1], prob=0.5),
     ScaleIntensityd(keys=["t1", "t2"]),
     ToTensord(keys=["t1", "t2", "roi"])
     ])
 
 trainset = RenalDataset(data_path, transform=transform)
-testset = RenalDataset(data_path, split=test)
+testset = RenalDataset(data_path, split="test")
 
-data = trainset[8]
-sample, labels = data["sample"], data["labels"]
-print(type(sample))
-print(type(labels))
-# Net = MultiLevelResNet(mixup=[0., 2., 2., 2.])
+net = MultiLevelResNet(mixup=[0., 2., 2., 2.]).to("cuda:0")
+
+summary(net, (3, 64, 64, 16))
