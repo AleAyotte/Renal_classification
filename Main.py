b06cfa7a1b924f65be00fd08ed3da70bdfc63ea7
Alexandre Ayotte <alexandre.ayotte2@Usherbrooke.ca>
2020-12-15 20:15:28 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     transform = Compose([
         AddChanneld(keys=["t1", "t2", "roi"]),
         RandFlipd(keys=["t1", "t2", "roi"], spatial_axis=[0, 1], prob=0.5),
-        ScaleIntensityd(keys=["t1", "t2"], factors=0.1, prob=0.5),
+        RandScaleIntensityd(keys=["t1", "t2"], factors=0.1, prob=0.5),
         # RandGaussianSharpend(keys=["t1", "t2"], prob=0.3)
         ToTensord(keys=["t1", "t2", "roi"])
         ])
