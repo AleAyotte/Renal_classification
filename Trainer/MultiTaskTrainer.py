01eb6993e8021ea2bd16a5bb08d11b8e12e61e12
Alexandre Ayotte <alexandre.ayotte2@Usherbrooke.ca>
2020-12-19 13:58:40 -0500

    MultiTrainer: Validation step fix

--- Trainer/MultiTaskTrainer.py
+++ Trainer/MultiTaskTrainer.py
@@ -316,10 +316,10 @@ class MultiTaskTrainer(Trainer):
             g_reccal = compute_recall(g_conf)
             
             m_acc = get_mean_accuracy(m_reccal, geometric_mean=True)
-            s_acc = get_mean_accuracy(s_reccal[0:2], geometric_mean=True)
-            g_acc = get_mean_accuracy(g_reccal[0:2], geometric_mean=True)
+            s_acc = get_mean_accuracy(s_reccal[1:], geometric_mean=True)
+            g_acc = get_mean_accuracy(g_reccal[1:], geometric_mean=True)
             
-            mean_acc = get_mean_accuracy([m_acc, s_acc, g_acc], geometric_mean=False)
+            mean_acc = get_mean_accuracy([m_acc, s_acc, g_acc], geometric_mean=True)
 
         if self._track_mode != "none":
             self._writer.add_scalars('{}/Accuracy'.format(dataset_name), 
