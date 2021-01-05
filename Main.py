edaf6e8dc7a8c9581a494dc58d7fe9fdada3926f
AleAyotte <alexandre.ayotte2@usherbrooke.ca>
2021-01-05 13:20:25 -0500

    Main: New argparse option added and crop and pad added

--- Main.py
+++ Main.py
@@ -3,6 +3,8 @@ from Data_manager.DataManager import RenalDataset, split_trainset
 from matplotlib import pyplot as plt
 from Model.ResNet import MultiLevelResNet
 from monai.transforms import RandFlipd, RandScaleIntensityd, ToTensord, Compose, AddChanneld, RandGaussianSharpend
+from monai.transforms import RandSpatialCropd, SpatialPadd
+import numpy as np
 from torchsummary import summary
 from Trainer.MultiTaskTrainer import MultiTaskTrainer as Trainer
 from Trainer.Utils import compute_recall
@@ -10,32 +12,39 @@ from Trainer.Utils import compute_recall
 
 def argument_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--device', type=str, default="cuda:0")
+    parser.add_argument('--activation', type=str, default='ReLU',
+                        choices=['ReLU', 'PReLU', 'LeakyReLU', 'Swish', 'ELU'])
+    parser.add_argument('--b_size', type=int, default=32)
     parser.add_argument('--dataset', type=str, default='Option1_with_N4',
                         choices=['Option1_with_N4', 'Option1_without_N4',
                                  'Option2_with_N4', 'Option2_without_N4'])
-    parser.add_argument('--num_epoch', type=int, default=100)                             
-    parser.add_argument('--mode', type=str, default="Mixup",
-                        choices=["standard", "Mixup"])
-    parser.add_argument('--warm_up', type=int, default=0)
-    parser.add_argument('--mixup', type=int, action='store', nargs="*", default=[0, 2, 2, 2])
-    parser.add_argument('--loss', type=str, default="ce",
-                        choices=["ce", "bce", "focal"])
-    parser.add_argument('--weights', type=str, default="balanced",
-                        choices=["flat", "balanced", "focused"])                        
-    parser.add_argument('--worker', type=int, default=0)
-    parser.add_argument('--optim', type=str, default="adam",
-                        choices=["adam", "novograd"])
-    parser.add_argument('--lr', type=float, default=1e-4)
-    parser.add_argument('--eps', type=float, default=1e-4)
+    parser.add_argument('--depth', type=int, default=18, choices=[18, 34, 50])
+    parser.add_argument('--device', type=str, default="cuda:0")
     parser.add_argument('--dropout', type=float, default=0)
     parser.add_argument('--drop_type', type=str, default="flat",
                         choices=["flat", "linear"])
+    parser.add_argument('--eps', type=float, default=1e-4)
+    parser.add_argument('--extra_data', type=bool, default=False)
+    parser.add_argument('--in_channels', type=int, default=16)
+    parser.add_argument('--loss', type=str, default="ce",
+                        choices=["ce", "bce", "focal"])
+    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--mixup', type=int, action='store', nargs="*", default=[0, 2, 2, 2])
+    parser.add_argument('--mode', type=str, default="Mixup",
+                        choices=["standard", "Mixup"])
+    parser.add_argument('--num_epoch', type=int, default=100)
+    parser.add_argument('--optim', type=str, default="adam",
+                        choices=["adam", "novograd"])
+    parser.add_argument('--pad_mode', type=str, default="constant",
+                        choices=["constant", "edge", "reflect", "symmetric"])
+    parser.add_argument('--pin_memory', type=bool, default=False)
+    parser.add_argument('--split_level', type=int, default=4)
     parser.add_argument('--track_mode', type=str, default="all",
                         choices=["all", "low", "none"])
-    parser.add_argument('--b_size', type=int, default=32)
-    parser.add_argument('--in_channels', type=int, default=16)
-    parser.add_argument('--pin_memory', type=bool, default=False)
+    parser.add_argument('--warm_up', type=int, default=0)
+    parser.add_argument('--weights', type=str, default="balanced",
+                        choices=["flat", "balanced", "focused"])                        
+    parser.add_argument('--worker', type=int, default=0)
     return parser.parse_args()
 
 
@@ -50,6 +59,8 @@ if __name__ == "__main__":
         RandFlipd(keys=["t1", "t2", "roi"], spatial_axis=[0, 1], prob=0.5),
         RandScaleIntensityd(keys=["t1", "t2"], factors=0.2, prob=0.5),
         # RandGaussianSharpend(keys=["t1", "t2"], prob=0.3),
+        RandSpatialCropd(keys=["t1", "t2", "roi"], roi_size=[64, 64, 16], random_center=False),
+        SpatialPadd(keys=["t1", "t2", "roi"], spatial_size=[96, 96, 32], mode=args.pad_mode),
         ToTensord(keys=["t1", "t2", "roi"])
     ])
 
@@ -61,17 +72,28 @@ if __name__ == "__main__":
     trainset = RenalDataset(data_path, transform=transform)
     validset = RenalDataset(data_path, transform=test_transform, split=None)
     testset = RenalDataset(data_path, transform=test_transform, split="test")
-    trainset, validset = split_trainset(trainset, validset)
+
+    if args.extra_data:
+        trainset2 = RenalDataset(data_path, transform=transform, split="test2")
+        data, label, _ = trainset2.extract_data(np.arange(len(trainset2)))
+        trainset.add_data(data, label)
+        del data
+        del label
+
+    trainset, validset = split_trainset(trainset, validset, validation_split=0.1)
 
     in_shape = tuple(trainset[0]["sample"].size()[1:])
     net = MultiLevelResNet(mixup=args.mixup,
+                           depth=args.depth,
+                           split_level=args.split_level,
                            in_shape=in_shape,
                            first_channels=args.in_channels,
                            drop_rate=args.dropout,
-                           drop_type=args.drop_type).to(args.device)
+                           drop_type=args.drop_type,
+                           act=args.activation).to(args.device)
 
     summary(net, (3, 96, 96, 32))
-    trainer = Trainer(save_path="Check_moi_ca.pth", 
+    trainer = Trainer(save_path="Check_moi_ca2.pth",
                       loss=args.loss,
                       tol=0.05,
                       num_workers=args.worker,
