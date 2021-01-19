8679283850044f58639b60e073e05729b79dba29
Alexandre Ayotte <alexandre.ayotte2@USherbrooke.ca>
2021-01-18 23:03:34 -0500

    RenalDataset: Refactor the clinical data can now be used with the RenalDataset.

--- Data_manager/DataManager.py
+++ Data_manager/DataManager.py
@@ -1,8 +1,7 @@
 import h5py
 import numpy as np
 import torch
-from torch.utils.data import Dataset, DataLoader
-from torch.utils.data.sampler import SubsetRandomSampler
+from torch.utils.data import Dataset
 from typing import Sequence, Tuple, Union
 
 
@@ -10,29 +9,38 @@ class RenalDataset(Dataset):
     """
     Renal classification dataset.
     """
-    def __init__(self, hdf5_filepath: str,
+    def __init__(self,
+                 hdf5_filepath: str,
+                 imgs_keys: Union[Sequence[str], str],
                  split: Union[str, None] = "train",
                  transform=None,
-                 load_clinical: bool = False):
+                 clinical_features: Union[Sequence[str], str] = None):
         """
         Create a dataset by loading the renal image at the given path.
 
         :param hdf5_filepath: The filepath of the hdf5 file where the data has been stored.
+        :param imgs_keys: The images name in the hdf5 file that will be load in the dataset (Exemple: "t1").
         :param split: A string that indicate which subset will be load. (Option: train, test, test2).
         :param transform: A function/transform that will be applied on the images and the ROI.
-        :param load_clinical: A boolean that indicate if dataset will also contain the clinical data.
+        :param clinical_features: A list of string that indicate which clinical features will be used
+                                  to train the model.
         """
         assert split in ['train', 'test', 'test2', None]
         self.transform = transform
-        self.__with_clinical = load_clinical
-        self.__data = []
-        self.__clinical_data = []
-        self.__label = []
+        self.__with_clinical = clinical_features is not None
+        self.__imgs_keys = imgs_keys
+        self.__data = np.array([])
+        self.__clinical_data = np.array([])
+        self.__labels = np.array([])
 
         if split is not None:
-            self.__read_hdf5(hdf5_filepath, split)
+            if self.__with_clinical and type(clinical_features) is not list:
+                clinical_features = [clinical_features]
 
-    def add_data(self, data: Sequence[dict],
+            self.__read_hdf5(hdf5_filepath, split, clinical_features)
+
+    def add_data(self,
+                 data: Sequence[dict],
                  label: Union[Sequence[dict], Sequence[int]],
                  clinical_data: Sequence[Sequence[int]] = None) -> None:
         """
@@ -42,13 +50,14 @@ class RenalDataset(Dataset):
         :param label: A sequence of dictionary or a sequence of int that contain the labels.
         :param clinical_data: A sequence of sequence of int that contain the clinical data.
         """
-        self.__data.extend(data)
-        self.__label.extend(label)
+        self.__data = np.append(self.__data, data, 0)
+        self.__labels = np.append(self.__labels, label, 0)
 
         if clinical_data is not None:
-            self.__clinical_data.extend(clinical_data)
+            self.__clinical_data = np.append(self.__clinical_data, clinical_data, 0)
 
-    def extract_data(self, idx: Sequence[int],
+    def extract_data(self,
+                     idx: Sequence[int],
                      pop: bool = True) -> Tuple[Sequence[dict],
                                                 Union[Sequence[dict], Sequence[int]],
                                                 Sequence[Sequence[int]]]:
@@ -59,49 +68,58 @@ class RenalDataset(Dataset):
         :param pop: If True, the extracted data are removed from the dataset. (Default: True)
         :return: A tuple that contain the data (images), the labels and the clinical data.
         """
-        clin = None
-        data = [self.__data[i] for i in idx]
-        label = [self.__label[i] for i in idx]
-        if self.__with_clinical:
-            clin = [self.__clinical_data[i] for i in idx]
+        mask = np.ones(len(self.__data), dtype=bool)
+        mask[idx] = False
+
+        data = self.__data[mask]
+        labels = self.__labels[mask]
+        clin = self.__clinical_data[mask] if self.__with_clinical else None
 
         if pop:
-            for i in sorted(idx, reverse=True):
-                del self.__data[i]
-                del self.__label[i]
-                if self.__with_clinical:
-                    del self.__clinical_data[i]
+            self.__data = self.__data[~mask]
+            self.__labels = self.__labels[~mask]
+            self.__clinical_data = self.__clinical_data[~mask] if self.__with_clinical else None
 
-        return data, label, clin
+        return data, labels, clin
 
-    def __read_hdf5(self, filepath: str,
-                    split: str) -> None:
+    def __read_hdf5(self,
+                    filepath: str,
+                    split: str,
+                    features_names: Sequence[str]) -> None:
         """
         Read the images and ROI from a given hdf5 filepath and store it in memory
 
         :param filepath: The filepath of the hdf5 file where the data has been stored.
         :param split: A string that indicate which subset will be load. (Option: train, test, test2).
+        :param features_names: A list of string that indicate which clinical features will be used
+                               to train the model.
         """
         f = h5py.File(filepath, 'r')
         dtset = f[split]
-        clinical_data = []
+        self.__data = []
+        self.__labels = []
+        self.__clinical_data = []
 
         for key in list(dtset.keys()):
-            self.__data.append({"t1": dtset[key]["t1"][:],
-                                "t2": dtset[key]["t2"][:],
-                                "roi": dtset[key]["roi"][:]})
+            imgs = {}
+            for img_key in list(self.__imgs_keys):
+                imgs[img_key] = dtset[key][img_key][:]
+
+            self.__data.append(imgs)
 
             if "outcome" in list(dtset[key].attrs.keys()):
-                self.__label.append(dtset[key].attrs["outcome"])
+                self.__labels.append(dtset[key].attrs["outcome"])
             else:
-                self.__label.append({"malignant": dtset[key].attrs["malignant"],
-                                     "subtype": dtset[key].attrs["subtype"],
-                                     "grade": dtset[key].attrs["grade"]})
+                self.__labels.append({"malignant": dtset[key].attrs["malignant"],
+                                      "subtype": dtset[key].attrs["subtype"],
+                                      "grade": dtset[key].attrs["grade"]})
             if self.__with_clinical:
                 attrs = dtset[key].attrs
-                clinical_data.append([attrs[key] for key in list(attrs.keys) if key != "outcome"])
+                self.__clinical_data.append([attrs[feat_name] for feat_name in features_names])
 
-        self.__clinical_data = np.array(clinical_data)
+        self.__data = np.array(self.__data)
+        self.__labels = np.array(self.__labels)
+        self.__clinical_data = np.array(self.__clinical_data)
         f.close()
 
     def __len__(self) -> int:
@@ -114,44 +132,38 @@ class RenalDataset(Dataset):
         sample = self.transform(self.__data[idx])
 
         if type(idx) == int:
-            # Stack the images into torch tensor
-            if len(sample["t1"].size()) == 4:
-                sample = torch.cat((sample["t1"], sample["t2"], sample["roi"]), 0)
-            else:
-                sample = torch.stack([sample["t1"], sample["t2"], sample["roi"]])
+            idx = [idx]
+            sample = [sample]
 
-            # Transfer the label in a dictionnary of torch tensor
-            if type(self.__label[idx]) == dict:
-                labels = {}
-                for key, value in self.__label[idx].items():
-                    labels[key] = torch.tensor(value).long()
+        # Stack or cat the images into one tensor.
+        # If one dimension has been added to the images, we need to cat them.
+        to_cat = next(iter(sample[0].values())).dim() > next(iter(self.__data[0].values())).ndim
+        temp = []
 
-            # Transfer the label in a torch tensor
+        for samp in sample:
+            if to_cat:
+                temp.append(torch.cat(tuple(samp[img_key] for img_key in self.__imgs_keys), 0))
             else:
-                labels = torch.tensor(self.__label[idx]).long()
+                temp.append(torch.stack([samp[img_key] for img_key in self.__imgs_keys]))
+        sample = torch.stack(temp) if len(temp) > 1 else temp[0]
+
+        # Extract and stack the labels in a dictionnary of torch tensor
+        if type(self.__labels[0]) == dict:
+            labels = {}
+            for key in list(self.__labels[idx][0].keys()):
+                labels[key] = torch.tensor([
+                    _dict[key] for _dict in self.__labels[idx]
+                ]).long().squeeze()
+        # Extract and stack the labels in a torch tensor
+        else:
+            labels = torch.tensor(self.__labels[idx]).long().squeeze()
+
+        if self.__with_clinical:
+            features = torch.tensor(self.__clinical_data[idx])
+            return {"sample": sample, "labels": labels, "features": features}
 
         else:
-            # Stack the images into torch tensor
-            temp = []
-            for samp in sample:
-                if len(samp["t1"].size()) == 4:
-                    temp.append(torch.cat((samp["t1"], samp["t2"], samp["roi"]), 0))
-                else:
-                    temp.append(torch.stack([samp["t1"], samp["t2"], samp["roi"]]))
-            sample = torch.stack(temp)
-
-            # Extract and stack the labels in a dictionnary of torch tensor
-            if type(self.__label[idx][0]) == dict:
-                labels = {}
-                for key in list(self.__label[idx][0].keys()):
-                    labels[key] = torch.tensor([
-                        _dict[key] for _dict in self.__label[idx]
-                    ]).long
-
-            # Extract and stack the labels in a torch tensor
-            else:
-                labels = torch.tensor(self.__label[idx]).long()
-        return {"sample": sample, "labels": labels}
+            return {"sample": sample, "labels": labels}
 
 
 def split_trainset(trainset: RenalDataset,
