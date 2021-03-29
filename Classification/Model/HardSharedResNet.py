94433a52dde84fabb0b84ccf0aa1c7120028ca68
AleAyotte <alexandre.ayotte2@usherbrooke.ca>
2021-03-29 13:42:00 -0400

    HardSharedResNet has been adapted to the recent change in MultiTaskTrainer and move into a new file.

--- Classification/Model/HardSharedResNet.py
+++ Classification/Model/HardSharedResNet.py
@@ -1,12 +1,11 @@
 """
-    @file:              ResNet.py
+    @file:              HardSharedResNet.py
     @Author:            Alexandre Ayotte
 
-    @Creation Date:     12/2020
+    @Creation Date:     03/2021
     @Last modification: 03/2021
 
-    @Description:       This file contain the classe ResNet that inherit from the NeuralNet class. This ResNet version
-                        can only be use for single task trainer on 3D images.
+    @Description:       This file contain the classe HardSharedResNet that inherit from the NeuralNet class.
 
     @Reference:         1) Identity Mappings in Deep Residual Networks, He, K. et al., ECCV 2016
 """
@@ -18,12 +17,12 @@ import numpy as np
 from Trainer.Utils import init_weights
 import torch
 import torch.nn as nn
-from typing import Sequence, Tuple, Union
+from typing import Dict, Sequence, Tuple, Union
 
 
-class ResNet(NeuralNet):
+class HardSharedResNet(NeuralNet):
     """
-    A ResNet 3D implementation for single task training.
+    A hard shared 3D Residual Network implementation for multi task learning.
 
     ...
     Attributes
@@ -31,8 +30,10 @@ class ResNet(NeuralNet):
     conv: Convolution
         First block of the network. If pre_act is True then, its only a convolution. Else, its combination of
         convolution, activation et normalisation.
-    fc_layer: nn.Linear
-        The last fully connected layer.
+    fc_layer_mal: nn.Linear
+        The last fully connected layer that will be used to classify the malignity of the tumor.
+    fc_layer_sub_1: nn.Linear
+        The first fully connected layer that will be used to classify the subtype of the tumor.
     __in_channels: int
         Number of output channels of the last convolution created. Used to determine the number of input channels of
         the next convolution to create.
@@ -48,6 +49,10 @@ class ResNet(NeuralNet):
         A dictionnary that contain all the mixup module.
     __num_flat_features: int
         Number of features at the output of the last convolution.
+    __split: int
+        Indicate where the network will be split to a multi-task network. Should be an integer between 1 and 5.
+        1 indicate that the network will be split before the first series of residual block.
+        5 indicate that the network will be split after the last series of residual block.
     Methods
     -------
     forward(x: torch.Tensor) -> torch.Tensor
@@ -61,24 +66,32 @@ class ResNet(NeuralNet):
         modules will be disable.
     """
     def __init__(self,
+                 tasks: Sequence[str],
+                 num_classes: Dict[str, int],
                  depth: int = 18,
                  first_channels: int = 16,
-                 num_classes: int = 2,
+                 split_level: int = 4,
                  in_shape: Union[Sequence[int], Tuple] = (64, 64, 16),
                  first_kernel: Union[Sequence[int], int] = 3,
                  kernel: Union[Sequence[int], int] = 3,
-                 mixup: Sequence[int] = None,
+                 mixup: Sequence[float] = None,
                  drop_rate: float = 0,
                  drop_type: str = "flat",
                  act: str = "ReLU",
                  norm: str = "batch",
                  pre_act: bool = True):
         """
-        Create a pre activation or post activation 3D Residual Network.
+        Create a pre activation or post activation 3D Residual Network for multi-task learning.
 
+        :param tasks: A list of tasks on which the model will be train.
         :param depth: The number of convolution and fully connected layer in the neural network. (Default=18)
         :param first_channels: The number of channels at the output of the first convolution layer. (Default=16)
-        :param num_classes: The number of features at the output of the neural network. (Default=2)
+        :param split_level: At which level the multi level resnet should split into sub net. (Default=4)
+                                1: After the first convolution,
+                                2: After the first residual level,
+                                3: After the second residual level,
+                                4: After the third residual level,
+                                5: After the last residual level so just before the fully connected layers.
         :param in_shape: The image shape at the input of the neural network. (Default=(64, 64, 16))
         :param first_kernel: The kernel shape of the first convolution layer. (Default=3)
         :param kernel: The kernel shape of all convolution layer except the first one. (Default=3)
@@ -94,7 +107,31 @@ class ResNet(NeuralNet):
         :param pre_act: If true, the PreResBlock or the PreResBottleneck will be used instead of ResBlock or
                         ResBottleneck. (Defaut=True)
         """
+        assert len(tasks) > 0, "You should specify the name of each task"
         super().__init__()
+        self.__split = split_level
+        self.__in_channels = first_channels
+        self.__tasks = tasks
+        nb_task = len(tasks)
+
+        # --------------------------------------------
+        #                NUM_CLASSES
+        # --------------------------------------------
+        # If num_classes has not been defined, then we assume that every task are binary classification.
+        if num_classes is None:
+            num_classes = {}
+            for task in self.__tasks:
+                num_classes[task] = 2
+
+        # If num_classes has been defined for some tasks but not all, we assume that the remaining are regression task
+        else:
+            key_set = set(num_classes.keys())
+            tasks_set = set(self.__tasks)
+            missing_tasks = tasks_set - key_set
+            assert missing_tasks == (tasks_set ^ key_set), f"The following tasks are present in num_classes " \
+                                                           "but not in tasks {}".format(key_set - tasks_set)
+            for task in list(missing_tasks):
+                num_classes[task] = 1
 
         # --------------------------------------------
         #                   MIXUP
@@ -105,6 +142,11 @@ class ResNet(NeuralNet):
         for i in range(len(mixup)):
             if mixup[i] > 0:
                 self.mixup[str(i)] = Mixup(mixup[i])
+
+        # --------------------------------------------
+        #              UNCERTAINTY LOSS
+        # --------------------------------------------
+        self.uncertainty_loss = UncertaintyLoss(num_task=nb_task)
 
         # --------------------------------------------
         #                    BLOCK
@@ -137,7 +179,7 @@ class ResNet(NeuralNet):
         # --------------------------------------------
         #                  CONV LAYERS
         # --------------------------------------------
-        self.__in_channels = first_channels
+        assert 1 <= split_level <= 5, "The split level should be an integer between 1 and 5."
         self.conv = Convolution(dimensions=3,
                                 in_channels=3,
                                 out_channels=self.__in_channels,
@@ -148,22 +190,30 @@ class ResNet(NeuralNet):
         self.layers1 = self.__make_layer(block[depth], layers[depth][0],
                                          first_channels, kernel=kernel,
                                          strides=[2, 2, 1], norm=norm,
-                                         drop_rate=dropout[0], act=act)
+                                         drop_rate=dropout[0], act=act,
+                                         split_layer=(1 == split_level),
+                                         groups=nb_task if 1 >= split_level else 1)
 
         self.layers2 = self.__make_layer(block[depth], layers[depth][1],
                                          first_channels * 2, kernel=kernel,
                                          strides=[2, 2, 2], norm=norm,
-                                         drop_rate=dropout[1], act=act)
+                                         drop_rate=dropout[1], act=act,
+                                         split_layer=(2 == split_level),
+                                         groups=nb_task if 2 >= split_level else 1)
 
         self.layers3 = self.__make_layer(block[depth], layers[depth][2],
                                          first_channels * 4, kernel=kernel,
                                          strides=[2, 2, 2], norm=norm,
-                                         drop_rate=dropout[2], act=act)
+                                         drop_rate=dropout[2], act=act,
+                                         split_layer=(3 == split_level),
+                                         groups=nb_task if 3 >= split_level else 1)
 
         self.layers4 = self.__make_layer(block[depth], layers[depth][3],
                                          first_channels * 8, kernel=kernel,
                                          strides=[2, 2, 2], norm=norm,
-                                         drop_rate=dropout[3], act=act)
+                                         drop_rate=dropout[3], act=act,
+                                         split_layer=(4 == split_level),
+                                         groups=nb_task if 4 >= split_level else 1)
 
         # --------------------------------------------
         #                   FC LAYERS
@@ -173,9 +223,15 @@ class ResNet(NeuralNet):
 
         self.avg_pool = nn.AvgPool3d(kernel_size=out_shape)
 
-        self.__num_flat_features = self.__in_channels
+        if self.__split == 5:
+            self.__num_flat_features = self.__in_channels
+        else:
+            self.__num_flat_features = int(self.__in_channels / nb_task)
 
-        self.fc_layer = nn.Linear(self.__num_flat_features, num_classes)
+        self.fc_layer = nn.ModuleDict()
+        for task in tasks:
+            self.fc_layer[task] = torch.nn.Linear(self.__num_flat_features,
+                                                  num_classes[task])
 
         self.apply(init_weights)
 
@@ -187,21 +243,27 @@ class ResNet(NeuralNet):
                      strides: Union[Sequence[int], int] = 1,
                      drop_rate: Sequence[float] = None,
                      act: str = "ReLU",
-                     norm: str = "batch") -> nn.Sequential:
-        layers = []
+                     norm: str = "batch",
+                     groups: int = 1,
+                     split_layer: bool = False) -> nn.Sequential:
 
+        fmap_out = fmap_out * groups
+        layers = []
         for i in range(num_block):
             layers.append(block(fmap_in=self.__in_channels, fmap_out=fmap_out,
                                 kernel=kernel,
                                 strides=strides if i == 0 else 1,
                                 drop_rate=drop_rate[i],
                                 activation=act,
-                                norm=norm))
+                                norm=norm,
+                                groups=groups,
+                                split_layer=split_layer))
+            split_layer = False
             self.__in_channels = fmap_out * block.expansion if i == 0 else self.__in_channels
 
         return nn.Sequential(*layers)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> Dict[str, torch.Tensor]:
         mixup_key_list = list(self.mixup.keys())
 
         out = self.mixup["0"](x) if "0" in mixup_key_list else x
@@ -218,7 +280,17 @@ class ResNet(NeuralNet):
         out = self.layers4(out)
         out = self.avg_pool(out)
 
-        features = out.view(-1, self.__num_flat_features)
-        out = self.fc_layer(features)
+        preds = {}
+        if self.__split == 5:
+            features = out.view(-1, self.__num_flat_features)
 
-        return out
+            for task in self.__tasks:
+                preds[task] = self.fc_layer[task](features)
+
+        else:
+            features = out.view(-1, len(self.__tasks), self.__num_flat_features)
+
+            for count, task in enumerate(self.__tasks):
+                preds[task] = self.fc_layer[task](features[:, count, :])
+
+        return preds
