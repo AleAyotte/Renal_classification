bf611b35b28ff4dcab57b5a6154fde654385137f
Alexandre Ayotte <alexandre.ayotte2@Usherbrooke.ca>
2021-07-27 19:12:39 -0400

    Create test_hardsharing.sh

---
+++ Classification/test_hardsharing.sh
@@ -0,0 +1,40 @@
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --grade
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --grade
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --grade
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --grade
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --grade
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --grade
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --grade
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --grade
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --grade
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --grade
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --subtype
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --subtype
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --subtype
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --subtype
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --subtype
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --subtype
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --subtype
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --subtype
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --subtype
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --subtype
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --subtype --grade
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --subtype --grade
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --subtype --grade
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --subtype --grade
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --subtype --grade
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --subtype --grade
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --subtype --grade
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --subtype --grade
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --subtype --grade
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --malignancy --subtype --grade
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --grade --subtype
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --grade --subtype
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --grade --subtype
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --grade --subtype
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --grade --subtype
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --grade --subtype
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --grade --subtype
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --grade --subtype
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --grade --subtype
+python SharedNetMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --depth_config=3 --split_level=3 --grade --subtype
