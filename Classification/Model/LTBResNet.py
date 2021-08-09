f5f4e09861d7bf2e64b8818355883f636d26bb75
Alexandre Ayotte <alexandre.ayotte2@Usherbrooke.ca>
2021-08-08 20:47:00 -0400

    Create LTBResNet.py

---
+++ Classification/Model/LTBResNet.py
@@ -0,0 +1,13 @@
+"""
+    @file:              LTBResNet.py
+    @Author:            Alexandre Ayotte
+
+    @Creation Date:     08/2021
+    @Last modification: 08/2021
+
+    @Description:       This file contain the class LTBResNet (Learn-To-Branch ResNet) that inherit from the NeuralNet
+                        class.
+
+    @Reference:         1) Identity Mappings in Deep Residual Networks, He, K. et al., ECCV 2016
+                        2) Learning to Branch for Multi-Task Learning, Guo, P. et al., CoRR 2020
+"""
