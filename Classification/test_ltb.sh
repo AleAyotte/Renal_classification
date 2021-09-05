fc9dfab90e8a28fb686a83a9a15f49d8d0b3e51e
AleAyotte <alexandre.ayotte2@usherbrooke.ca>
2021-09-04 20:44:15 -0400

    New bash file added for experimentation on LTBResNet.

---
+++ Classification/test_ltb.sh
@@ -0,0 +1,30 @@
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100 --branch_lr=1e-3
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100 --branch_lr=1e-3
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100 --branch_lr=1e-3
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100 --branch_lr=1e-3
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100 --branch_lr=1e-3
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100 --branch_lr=1e-3
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100 --branch_lr=1e-3
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100 --branch_lr=1e-3
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100 --branch_lr=1e-3
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100 --branch_lr=1e-3
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100 --branch_lr=1e-5
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100 --branch_lr=1e-5
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100 --branch_lr=1e-5
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100 --branch_lr=1e-5
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100 --branch_lr=1e-5
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100 --branch_lr=1e-5
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100 --branch_lr=1e-5
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100 --branch_lr=1e-5
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100 --branch_lr=1e-5
+python LTBMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4 --l2=3e-6 --width=2 --tau=10 --malignancy --subtype --b_size=16 --num_cumu_batch=2 --num_epoch=100 --branch_lr=1e-5
