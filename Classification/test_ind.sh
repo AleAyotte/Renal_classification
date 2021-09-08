bbd6d307d933fdcf69edbd53ff2af778c6e17bde
Alexandre Ayotte <alexandre.ayotte2@Usherbrooke.ca>
2021-09-08 17:04:08 -0400

    Minor fix

--- Classification/test_ind.sh
+++ Classification/test_ind.sh
@@ -1,40 +1,40 @@
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --subtype --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --subtype --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --subtype --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --subtype --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --subtype --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --subtype --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --subtype --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --subtype --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --subtype --grade
-python IndNormResNet.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --subtype --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --malignancy --subtype --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --subtype --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --subtype --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --subtype --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --subtype --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --subtype --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --subtype --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --subtype --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --subtype --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --subtype --grade
+python IndNormMain.py --drop_rate=0.3 --in_channels=32 --worker=8 --lr=1e-4  --l2=9e-3 --split_level=3 --b_size=16 --num_cumu_batch= 2 --subtype --grade
