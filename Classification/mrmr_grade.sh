1dd9c9412851d0d82c9cf66cc2c37b5d141b4e7c
Alexandre Ayotte <alexandre.ayotte2@Usherbrooke.ca>
2022-04-04 15:44:42 -0400

    Update mrmr_grade.sh

--- Classification/mrmr_grade.sh
+++ Classification/mrmr_grade.sh
@@ -1,20 +1,20 @@
-python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=12 --grade --config=1
-python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=12 --grade --config=1
-python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=12 --grade --config=1
-python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=12 --grade --config=1
-python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=12 --grade --config=1
-python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=12 --grade --config=1
-python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=12 --grade --config=1
-python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=12 --grade --config=1
-python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=12 --grade --config=1
-python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=12 --grade --config=1
-python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=13 --grade --config=1
-python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=13 --grade --config=1
-python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=13 --grade --config=1
-python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=13 --grade --config=1
-python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=13 --grade --config=1
-python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=13 --grade --config=1
-python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=13 --grade --config=1
-python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=13 --grade --config=1
-python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=13 --grade --config=1
-python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=13 --grade --config=1
+python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=12 --grade --config=1 --keep_unlabeled
+python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=12 --grade --config=1 --keep_unlabeled
+python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=12 --grade --config=1 --keep_unlabeled
+python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=12 --grade --config=1 --keep_unlabeled
+python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=12 --grade --config=1 --keep_unlabeled
+python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=12 --grade --config=1 --keep_unlabeled
+python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=12 --grade --config=1 --keep_unlabeled
+python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=12 --grade --config=1 --keep_unlabeled
+python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=12 --grade --config=1 --keep_unlabeled
+python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=12 --grade --config=1 --keep_unlabeled
+python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=13 --grade --config=1 --keep_unlabeled
+python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=13 --grade --config=1 --keep_unlabeled
+python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=13 --grade --config=1 --keep_unlabeled
+python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=13 --grade --config=1 --keep_unlabeled
+python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=13 --grade --config=1 --keep_unlabeled
+python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=13 --grade --config=1 --keep_unlabeled
+python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=13 --grade --config=1 --keep_unlabeled
+python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=13 --grade --config=1 --keep_unlabeled
+python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=13 --grade --config=1 --keep_unlabeled
+python main.py hs --worker=8 --aux_coeff=0.25 --split_level=4 --in_channels=32 --drop_rate=0.3 --l2=3e-6 --lr=1e-4 --b_size=8 --num_cumu_batch=4 --aux_task_set=13 --grade --config=1 --keep_unlabeled
